52d3f98418c53584aedb19be55aae9a9ec4f43a8
Ayakay <120978042+Ayaraky@users.noreply.github.com>
2024-08-09 09:45:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,9 @@
-body {
+const avanca = document.querySelectorAll.apply('.proximo-bt');
+avanca.forEach(button => {
+    button.addEventListener('click', function () {
 
+        const atual - document.querySelectorAll('.ativo');
 
-    background: black;
-    color: white;
+    })
 
-
-    font-family: "Montagu Slab", serif;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 700px;
-    margin: 0;
-
-}
-
-button {
-
-    background-color: rgb(155, 23, 34);
-
-    color: white;
-
-    font-family: "Montagu Slab", serif;
-
-    margin-left: 3%;
-
-
-
-    padding: 3px;
-
-
-}
+})
