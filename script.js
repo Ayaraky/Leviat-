41fd88cc8bcc7e8efdc33b3af8d3eec7c8c3c796
Ayakay <120978042+Ayaraky@users.noreply.github.com>
2024-08-09 09:21:31 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+body {
+
+
+    background: black;
+    color: white;
+
+
+    font-family: "Montagu Slab", serif;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 700px;
+    margin: 0;
+
+}
+
+button {
+
+    background-color: rgb(155, 23, 34);
+
+    color: white;
+
+    font-family: "Montagu Slab", serif;
+
+    margin-left: 3%;
+
+
+
+    padding: 3px;
+
+
+}
