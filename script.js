a55854aa4ff1859a50bcfe3c9dd52476469eba45
Ayakay <120978042+Ayaraky@users.noreply.github.com>
2024-08-12 21:23:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const avanca = document.querySelectorAll.apply('.proximo-bt');
+const avanca = document.querySelectorAll('.btn-proximo');
+
 avanca.forEach(button => {
-    button.addEventListener('click', function () {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-        const atual - document.querySelectorAll('.ativo');
-
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
-
 })
